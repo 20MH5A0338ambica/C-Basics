28445c77d66912d5ebd84858c32079dd9b9e8e88
20MH5A0338ambica <92074908+20MH5A0338ambica@users.noreply.github.com>
2021-10-23 10:50:39 +0530

    Add files via upload

---
+++ datatypes1.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+void main()
+{
+	// variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//intialization
+	age  = 17;
+	cgpa = 9.2;
+	initial ='t';
+	
+	//output
+	printf( "age of the student is %d\n",age);
+	printf( "cgpa of the student isn %f\n",cgpa);
+	printf( "student surname starts with %c",initial);
+}
---
+++ task1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	// varaiable declaration
+	float cgpa1, cgpa2, sum;
+	
+	// initialzation
+	cgpa1 = 8.3;
+	cgpa2 = 7.5;
+	
+	// calculation
+	sum = cgpa1+cgpa2;
+	
+	//output
+	printf(" sum of %f and %f is %f" , cgpa1,cgpa2,sum);
+}
