6386329ab7b0f8d1a6433fefcf2bde5c95b95886
20MH5A0338ambica <92074908+20MH5A0338ambica@users.noreply.github.com>
2021-10-28 10:44:47 +0530

    Add files via upload

---
+++ marks.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int phy, che, mat, eng, tot;
+	
+	// initialization
+	
+	phy = 67;
+	che = 78;
+	mat = 56;
+	eng = 78;
+	
+	// calculation
+	
+	tot = phy+che+mat+eng;
+	
+	// output
+	
+	printf("marks in phy:%d\n",phy);
+	printf("marks in che:%d\n",che);
+	printf("marks in mat:%d\n",mat);
+	printf("marks in eng:%d\n",eng);
+	printf("tot marks:%d",tot);
+}
---
+++ rules of variables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	// variable declaration
+	
+	int _beck_2710;
+	
+	// initilazation
+	
+	_beck_2710 = 8.5;
+	
+	// output
+	
+	printf("%d",_beck_2710);
+}
