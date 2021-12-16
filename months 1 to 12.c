152aa952acffe6f5e7371e0b799df1aaefe26c1e
20MH5A0338ambica <92074908+20MH5A0338ambica@users.noreply.github.com>
2021-12-16 10:48:34 +0530

    Add files via upload

---
+++ months 1 to 12.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+void main()
+{
+	int number;
+	printf("%d", &number);
+	scanf("%d", &number);
+	if (number == 1)
+	{
+		printf("january");
+	}
+	else if (number == 2)
+	{
+		printf("february");
+	}
+	else if (number == 3)
+	{
+		printf("march");
+	}
+	else if (number == 4)
+	{
+		printf("april");
+	}
+	else if (number == 5)
+	{
+		printf("may");
+	}
+	else if (number == 6)
+	{
+		printf("june");
+	}
+	else if (number == 7)
+	{
+		printf("july");
+	}
+	else if (number == 8)
+	{
+		printf("augest");
+	}
+	else if (number == 9)
+	{
+		printf("september");
+	}
+	else if (number == 10)
+	{
+		printf("octumber");
+	}
+	else if (number == 11)
+	{
+		printf("november");
+	}
+	else if (number == 12)
+	{
+		printf("december");
+	}
+	else 
+	{
+		printf("ambica is the new king");
+	}
+}
